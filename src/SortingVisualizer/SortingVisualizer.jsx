8eb4d9c9233f8a61f98d4a402ffd87fc64cd6191
Kalp <kalpshah2003@gmail.com>
2022-06-23 10:02:12 -0400

    initial commit

---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import * as sortingAlgorithm from '../sortingAlgorithms/sortingAlgorithms.js';
+import './SortingVisualizer.css';
+
+export default class SortingVisualizer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            array: [],
+        };
+    }
+
+    componentDidMount() {
+        this.resetArray();
+    }
+
+    resetArray() {
+        const array = [];
+        for (let i = 0; i < 310; i++) {
+            array.push(randomIntFromInterval(5, 730));
+        }
+        this.setState({array});
+    }
+
+    mergeSort() {
+        const animations = sortingAlgorithm.mergeSort(this.state.array);
+        for (let i = 0; i < animations.length; i++) {
+            const arrayBars = document.getElementsByClassName('array-bar');
+            const isColorChange = i % 3 !== 2;
+            if (isColorChange) {
+                const [barOneIndex, barTwoIndex] = animations[i];
+                const barOneStyle = arrayBars[barOneIndex].style;
+                const barTwoStyle = arrayBars[barTwoIndex].style;
+                const color = i % 3 === 0 ? 'blue' : 'pink';
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = color;
+                    barTwoStyle.backgroundColor = color;
+                }, i + 10);
+            } else {
+                setTimeout(() => {
+                    const [barOneIndex, newHeight] = animations[i];
+                    const barOneStyle = arrayBars[barOneIndex].style;
+                    barOneStyle.height = `${newHeight}px`;
+                }, i + 10);
+            }
+        }
+    }
+
+    quickSort() {}
+
+    heapSort() {}
+    
+    bubbleSort() {}
+
+    render() {
+        const {array} = this.state;
+        return (
+            <div className="array-container">
+                {array.map((value, index) => (
+                    <div className = "array-bar" key={index} style={{height: `${value}px`}}></div> 
+                ))}
+                <button onClick={() => this.resetArray()}>Generate New Array</button>
+                <button onClick={() => this.mergeSort()}>Merge Sort</button>
+                <button onClick={() => this.quickSort()}>Quick Sort</button>
+                <button onClick={() => this.heapSort()}>Heap Sort</button>
+                <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+            </div>
+        )
+    }
+}
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
